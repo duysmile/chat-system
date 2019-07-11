74145ad272db0b02f42e7eb50728f05bd1aa47c7
Duy Nguyen <duy210697@gmail.com>
2019-07-11 19:38:39 +0700

    change mongodb config

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = {
     mongoose,
     connectDB: function() {
-        return mongoose.connect('mongodb://localhost:27017/node03', { 
+        return mongoose.connect('mongodb://ur1hzptr8lpash8t00d1:pvr7emsXyBByjVtD6IgI@b3ie5yhakdturym-mongodb.services.clever-cloud.com:27017/b3ie5yhakdturym', { 
             useNewUrlParser: true,
             useFindAndModify: false,
             useCreateIndex: true
